e79ef3623b358503f258f1b2b2fb889a3b44f323
William Hatcher <william@hatcher.work>
2024-11-12 13:29:08 -0600

    debugging gh workflow

--- ci/copy-autoconfig.sh
+++ ci/copy-autoconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir -p $GITHUB_WORKSPACE/dist/.well-known/autoconfig/mail
+mkdir -p $GITHUB_WORKSPACE/dist/.well-known/autocnfig/mail
+ls $GITHUB_WORKSPACE
 
 cp $GITHUB_WORKSPACE/autoconfig.xml $GITHUB_WORKSPACE/dist/.well-known/autocnfig/mail/config-v1.1.xml
