cb52a623abcf75d700cb6d48e67465fa87780b01
William Hatcher <william@hatcher.work>
2024-11-12 12:59:53 -0600

    Actually updated copy-autoconfig.js file

--- ci/copy-autoconfig.js
+++ ci/copy-autoconfig.js
@@ -1,18 +1,23 @@
+#!/usr/bin/env node
+
 /* add thunderbird autoconfig xml to dist directory */
 const fs = require('fs');
 
+console.log("Creating dir");
 fs.mkdir('dist/.well-known/autoconfig/mail', {recursive: true}, (err) => {
-   if (err) throw err;
-   console.log("dir created");
+  if (err) throw err;
+  console.log("dir created");
 });
 
-// fs.readdir('.', (err, files) => {
-//     files.forEach(file => {
-//         console.log(file);
-//     });
-// });
+console.log("Listing files");
+fs.readdir('.', (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
 
+console.log("Copying autoconfig");
 fs.copyFile('./autoconfig.xml', 'dist/.well-known/autoconfig/mail/config-v1.1.xml', (err) => {
-    if (err) throw err;
-    console.log("Copied!");
+  if (err) throw err;
+  console.log("Copied!");
 });
